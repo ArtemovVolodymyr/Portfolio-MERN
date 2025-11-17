057e6baec71a8a1abed0dd28934e721dacbd3c05
ArtemovVolodymyr <volodymyr.artemov04@gmail.com>
2025-11-17 15:23:15 +0100

    Create header and home page add css

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import "./App.css";
 import Header from './components/Header/Header.jsx';
+import Home from './components/Home/Home.jsx';  
 
 
 const App = () => {
   return (
     <>
     <Header />
+
+    <main className="main">
+      <Home />
+    </main>
     </>
   )
 }
---
+++ src/components/Home/Social.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Social = () => {
+    return (
+        <div className="home__social">
+            <a href="https://www.instagram.com/artemov.vo/" className="home__social-icon" target='_blank'>
+                <i className="uil uil-instagram"></i>
+            </a>
+
+            <a href="https://www.linkedin.com/in/volodymyr-artemov/" className="home__social-icon" target='_blank'>
+                <i className="uil uil-linkedin"></i>
+            </a>
+
+            <a href="https://github.com/ArtemovVolodymyr" className="home__social-icon" target='_blank'>
+                <i className="uil uil-github-alt"></i>
+            </a>
+        </div>
+    )
+}
+
+export default Social
