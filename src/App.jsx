beb0539d89385cd48de0558a7d7f381f79ec969b
ArtemovVolodymyr <volodymyr.artemov04@gmail.com>
2025-11-19 14:56:25 +0100

    Create Skills component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import Header from './components/Header/Header.jsx';
 import Home from './components/Home/Home.jsx';  
 import About from './components/About/About.jsx';
+import Skills from './components/Skills/Skills.jsx';
 
 
 const App = () => {
@@ -13,6 +14,7 @@ const App = () => {
     <main className="main">
       <Home />
       <About />
+      <Skills />
     </main>
     </>
   )
